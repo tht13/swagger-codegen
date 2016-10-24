4ae12fc1151243243300b18640502467eca975c4
Thomas Townsend <thomas.haakon@gmail.com>
2016-10-24 22:02:39 +0100

    Add security support to typescript-angular generator

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptAngularClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptAngularClientCodegen.java
@@ -22,6 +22,7 @@ public class TypeScriptAngularClientCodegen extends AbstractTypeScriptClientCode
 		supportingFiles.add(new SupportingFile("api.d.mustache", apiPackage().replace('.', File.separatorChar), "api.d.ts"));
 		supportingFiles.add(new SupportingFile("git_push.sh.mustache", "", "git_push.sh"));
 		supportingFiles.add(new SupportingFile("gitignore", "", ".gitignore"));
+		supportingFiles.add(new SupportingFile("configuration.mustache", "", "configuration.ts"));
 
 	}
 	
